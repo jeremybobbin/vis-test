cc6c74bfdbe66e1674bf6e725469b53e74f84564
Jeremy Bobbin <jer@jer.cx>
2024-08-27 18:55:38 -0700

    parallelize test/{vis,vim}/test.sh
    examples:
    JOBS=$(nproc) ./test.sh
    JOBS=128 make test
    128 is a lot cooler bc of all the sleeping that happens
    
    set the DEBUG environment variable for what would otherwise be /dev/null
    to be printed. This means vis writes to stderr, which allows you to see
    the tests run
    
    for test/vim/test.sh, DEBUG=40 means 40 chars a second
    test output is written to test.out

--- vim/test.sh
+++ vim/test.sh
@@ -1,69 +1,109 @@
 #!/bin/sh
+export LANG="en_US.UTF-8"
+export VIS_PATH=.
+export EDITORS
 
 [ -z "$VIS" ] && VIS="../../vis"
 [ -z "$VIM" ] && VIM="vim"
 
-TESTS=$1
-[ -z "$TESTS" ] && TESTS=$(find . -name '*.keys' | sed 's/\.keys$//g')
-
-TESTS_RUN=0
-TESTS_OK=0
-TESTS_SKIP=0
-
-export LANG="en_US.UTF-8"
-export VIS_PATH=.
-
-if type "$VIM" >/dev/null 2>&1; then
+if [ -n "$EDITORS" ]; then
+	:
+elif command -v "$VIM" >/dev/null 2>&1; then
 	EDITORS="$VIM $VIS"
-	$VIM --version | head -1
 else
 	EDITORS="$VIS"
 fi
 
-$VIS -v
+if [ -n "$DEBUG" ]; then
+	exec 3>/dev/stderr
+else
+	exec 3>/dev/null
+fi
 
-for t in $TESTS; do
+if [ $# -eq 1 ]; then
 	for EDITOR in $EDITORS; do
-		e=$(basename "$EDITOR");
-		ERR="$t.$e.err"
-		OUT="$t.$e.out"
-		REF="$t.ref"
-		VIM_OUT="$t.$VIM.out"
-		printf "Running test %s with %s ... " "$t" "$e"
-		rm -f "$OUT" "$ERR"
-		[ "$e" = "$VIM" ] && EDITOR="$VIM --not-a-term -n -u NONE -U NONE -N -i NONE"
-		{ cat "$t.keys"; printf "<Escape>:w! $OUT<Enter>:qall!<Enter>\n"; } | ${CPP:-cpp} -P 2>/dev/null | sed 's/[ \t]*$//' | ../util/keys | $EDITOR "$t.in" >/dev/null 2>&1
-		if [ "$e" = "$VIM" ]; then
-			if [ -e "$REF" ]; then
-				if cmp -s "$REF" "$OUT"; then
-					printf "OK\n"
-				else
-					printf "FAIL\n"
-					diff -u "$REF" "$OUT" > "$ERR"
-				fi
-			elif [ -e "$VIM_OUT" ]; then
-				printf "OK\n"
+		e=$(basename "$EDITOR")
+		if
+			[ "$e" = "vis" ] && { ! [ -e "$1.ref" ] && ! [ -e "$1.vim.out" ]; }
+		then
+			# we're vis but we have no reference to compare it to
+			echo "$e $1 SKIP"
+			exit 0
+		elif
+			! sed 's,[ \t]*/\*.*\*/[ \t]*,,; $a\
+				<Escape>:w! '"$1.$e.out"'<Enter>:qall!<Enter>' "$1.keys" |
+			if [ -n "$DEBUG" ] && [ "$e" = "vis" ]; then
+				exec ../util/keys -L "$DEBUG"
 			else
-				printf "FAIL\n"
+				exec ../util/keys
+			fi | (
+				# escalate sigint bc vim & vis have their thumb up their ass - cannot be interupted
+				trap 'kill 0' INT TERM
+				rm -f "$1.$e.out"
+				case "$e" in
+					vim) $EDITOR --not-a-term -n -u NONE -U NONE -i NONE "$1.in" >/dev/null 2>&1;;
+					vis|*) $EDITOR "$1.in" 1>&3 2>&1
+				esac && ( [ -e "$1.$e.out" ] || ( printf "vis failed to produce output file\n" 1>&3; false ) )
+			)
+		then
+		        echo "$e $1 ERROR"
+		        if [ -n "$DEBUG" ]; then
+				exit 2
 			fi
-		elif [ -e "$REF" -o -e "$VIM_OUT" ]; then
-			[ -e "$VIM_OUT" ] && REF="$VIM_OUT"
-			if cmp -s "$REF" "$OUT"; then
-				printf "OK\n"
-				TESTS_OK=$((TESTS_OK+1))
-			else
-				printf "FAIL\n"
-				diff -u "$REF" "$OUT" > "$ERR"
-			fi
-			TESTS_RUN=$((TESTS_RUN+1))
+			exit 255
+		elif
+			{ ! [ -e "$1.ref" ] && [ "$e" != "vis" ]; } || # other editor produces the reference - if it ran, it passed
+			(
+				{ [ -e "$1.ref" ] && diff -u "$1.ref" "$1.$e.out"; } ||
+				{ [ "$e" = "vis" ] && [ -e "$1.vim.out" ] && diff -u "$1.vim.out" "$1.$e.out"; }
+			) > "$1.$e.err"
+		then
+
+		        echo "$e $1 PASS"
 		else
-			TESTS_SKIP=$((TESTS_SKIP+1))
-			printf "SKIPPED\n"
+		        echo "$e $1 FAIL"
+			exit 1
 		fi
 	done
-done
 
-printf "Tests ok %d/%d, skipped %d\n" $TESTS_OK $TESTS_RUN $TESTS_SKIP
+	exit 0
+else
+	find . -type f -name '*.keys' | sed 's,.keys$,,; s,^./,,' |
+		if  [ -n "$JOBS" ]  && [ "$JOBS" -gt 1 ] && command -v xargs >/dev/null 2>&1; then
+			RECURSIVE=1 xargs -n1 -P"$JOBS" "$0" 2>&3
+		else while read -r t; do
+			RECURSIVE=1 "$0" "$t"
+			if [ $? = 255 ]; then
+				break
+			fi
+		done; fi | tee test.out
+fi | if [ -n "$RECURSIVE" ]; then
+	cat
+else
+	{
+		$VIM --version | head -1
+		$VIS -v
+	} 1>&2 </dev/null
 
-# set exit status
-[ $TESTS_OK -eq $TESTS_RUN ]
+	if command -v tput >/dev/null 2>&1 && [ -t 1 ]; then
+		:
+	else
+		tput() {
+			:
+		}
+	fi
+
+	awk \
+		-v "red=$(tput setaf 1)" \
+		-v "green=$(tput setaf 2)" \
+		-v "white=$(tput setaf 7)" \
+		-v "yellow=$(tput setaf 3)" \
+		'
+			/PASS/  {p++; c=green  }
+			/SKIP/  {s++; c=yellow }
+			/FAIL/  {     c=red    }
+			/ERROR/ {     c=red    }
+			{ printf "%s %-50s %s\n", $1, $2, c $3 white }
+			END { n=NR-s; printf "Tests ok %d/%d, skipped %d\n", p, NR, s | "cat 1>&2"; exit p != n }
+		'
+fi
