bc6e1e1afdb3510f58f7f3c724512a15003b8c00
Jeremy Bobbin <jer@jer.cx>
2024-08-27 18:55:38 -0700

    parallelize test/{vis,vim}/test.sh
    examples:
    JOBS=$(nproc) ./test.sh
    JOBS=128 make test
    128 is a lot cooler bc of all the sleeping that happens
    
    set the DEBUG environment variable for what would otherwise be /dev/null
    to be printed. This means vis writes to stderr, which allows you to see
    the tests run
    
    for test/vim/test.sh, DEBUG=40 means 40 chars a second
    test output is written to test.out

--- vis/test.sh
+++ vis/test.sh
@@ -1,48 +1,71 @@
 #!/bin/sh
 
 export VIS_PATH=.
-export PATH="$(pwd)/../..:$PATH"
+export PATH="$PWD/../..:$PATH"
 export LANG="en_US.UTF-8"
 [ -z "$VIS" ] && VIS="../../vis"
-$VIS -v
+export VIS
 
-if ! $VIS -v | grep '+lua' >/dev/null 2>&1; then
-	echo "vis compiled without lua support, skipping tests"
-	exit 0
-fi
-
-TESTS_OK=0
-TESTS_RUN=0
-
-if [ $# -gt 0 ]; then
-	test_files=$*
+if [ -n "$DEBUG" ]; then
+	exec 3>/dev/stderr
 else
-	printf ':help\n:/ Lua paths/,$ w help\n:qall\n' | $VIS 2> /dev/null && cat help && rm -f help
-	test_files="$(find . -type f -name '*.in')"
+	exec 3>/dev/null
 fi
 
-for t in $test_files; do
-	TESTS_RUN=$((TESTS_RUN + 1))
-	t=${t%.in}
-	t=${t#./}
-	$VIS '+qall!' "$t".in < /dev/null 2> /dev/null
-	RETURN_CODE=$?
 
-	printf "%-50s" "$t"
-	if [ $RETURN_CODE -eq 0 -a -e "$t".out ]; then
-		if cmp -s "$t".ref "$t".out 2> /dev/null; then
-			printf "PASS\n"
-			TESTS_OK=$((TESTS_OK + 1))
-		else
-			printf "FAIL\n"
-			diff -u "$t".ref "$t".out > "$t".err
+if [ $# -eq 1 ]; then
+	rm -f "$1.out"
+	if ! $VIS '+qall' "$1.in" < /dev/null 2>&3 || ! [ -e "$1.out" ]; then
+	        echo "$1 ERROR"
+	        if [ -n "$DEBUG" ]; then
+			exit 2
 		fi
+		exit 255
+	elif diff -u "$1.ref" "$1.out" > "$1.err"; then
+	        echo "$1 PASS"
+	        exit 0
 	else
-		printf "ERROR\n"
+	        echo "$1 FAIL"
+	        exit 1
 	fi
-done
+else
+	find . -type f -name '*.keys' | sed 's,.keys$,,; s,^./,,' |
+	if  [ -n "$JOBS" ]  && [ "$JOBS" -gt 1 ] && command -v xargs >/dev/null 2>&1; then
+		RECURSIVE=1 xargs -n1 -P"$JOBS" "$0" 2>&3
+	else while read -r t; do
+		RECURSIVE=1 "$0" "$t"
+		if [ $? = 255 ]; then
+			break
+		fi
+	done; fi | tee test.out
+fi | if [ -n "$RECURSIVE" ]; then
+	cat
+else
+	if ! $VIS -v 2>&1 | grep '+lua' >/dev/null; then
+		echo "vis compiled without lua support, skipping tests" 1>&2
+		exit 0
+	fi
 
-printf "Tests ok %d/%d\n" $TESTS_OK $TESTS_RUN
+	if ! command -v tput >/dev/null 2>&1 || ! [ -t 1 ]; then
+		tput() {
+			:
+		}
+	fi
 
-# set exit status
-[ $TESTS_OK -eq $TESTS_RUN ]
+	printf ':help\n:/ Lua paths/,$ w! help\n:qall\n' | $VIS >/dev/null 2>&1 && cat help 1>&2 && rm -f help
+	$VIS -v 1>&2
+
+	awk \
+		-v "red=$(tput setaf 1)" \
+		-v "green=$(tput setaf 2)" \
+		-v "white=$(tput setaf 7)" \
+		-v "yellow=$(tput setaf 3)" \
+		'
+			/PASS/  {p++; c=green  }
+			/SKIP/  {s++; c=yellow }
+			/FAIL/  {     c=red    }
+			/ERROR/ {     c=red    }
+			{ printf "%-50s %s\n", $1, c $2 white }
+			END { n=NR-s; printf "Tests ok %d/%d, skipped %d\n", p, NR, s | "cat 1>&2"; exit p != n }
+		'
+fi
